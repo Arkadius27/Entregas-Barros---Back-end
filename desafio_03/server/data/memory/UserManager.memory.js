4601f283ddf2a6bef4d6e3b1ae02bc3fc0619fd0
Agustin Barros <barroscarrasco.agustin@gmail.com>
2024-01-02 20:34:22 -0300

    Sprint3 (#3)
    * base
    
    * not working - json error
    
    * second commit - endpoints error
    
    * third commit - resolved issues

---
+++ desafio_03/server/data/memory/UserManager.memory.js
@@ -0,0 +1,62 @@
+import crypto from "crypto";
+
+class UserManager {
+  // static class properties
+  static #users = [];
+
+  constructor() {}
+
+  create(data) {
+    const user = {
+      id: crypto.randomBytes(12).toString("hex"),
+      name: data.name,
+      photo: data.photo,
+      email: data.email,
+    };
+
+    UserManager.#users.push(user);
+  }
+
+  read() {
+    return UserManager.#users;
+  }
+
+  readOne(id) {
+    return UserManager.#users.find((each) => each.id === id);
+  }
+
+  destroy(id) {
+    try {
+      const one = UserManager.#users.find((prod) => prod.id === id);
+      if (one) {
+        UserManager.#users = UserManager.#users.filter((prod) => prod.id !== id);
+        console.log("User deleted");
+        return one;
+      } else {
+        throw new Error("User not found");
+      }
+    } catch (error) {
+      console.log(error.message);
+      return error.message;
+    }
+  }
+}
+
+const users = new UserManager();
+users.create({
+  name: "Agustin",
+  photo: "./images/imgU1.png",
+  email: "user1.agustin@email.com",
+});
+users.create({
+  name: "Felipe",
+  photo: "./images/imgU2.png",
+  email: "user2.felipe@email.com",
+});
+users.create({
+  name: "Martin",
+  photo: "./images/imgU3.png",
+  email: "user3.martin@email.com",
+});
+
+console.log(users.read());
