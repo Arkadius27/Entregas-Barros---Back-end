bce4d8dc0942e1da01e7dfd588611443bababdba
Agustin Barros <barroscarrasco.agustin@gmail.com>
2024-01-16 23:55:42 -0300

    form w/ socket advancements

--- desafio_04/server/public/form.js
+++ desafio_04/server/public/form.js
@@ -3,12 +3,24 @@ const socket = io();
 socket.on("welcome", (message) => {
   alert(message);
 });
-socket.emit("new product", {
-  title: "Xbox Series S",
-  photo: "https://m.media-amazon.com/images/I/61f1agMOeTL.jpg",
-  price: 590,
-  stock: 7,
-});
+
 socket.on("new success", (message) => {
   alert(message);
+});
+
+document.querySelector("#submitProduct").addEventListener("click", (product) => {
+  product.preventDefault();
+  const title = document.querySelector("#title").value;
+  const photo = document.querySelector("#photo").value;
+  const price = document.querySelector("#price").value;
+  const stock = document.querySelector("#stock").value;
+  const data = {};
+  title && (data.title = title);
+  photo && (data.photo = photo);
+  price && (data.price = price);
+  stock && (data.stock = stock);
+
+  console.log(data);
+
+  // socket.emit("new product", data);
 });
